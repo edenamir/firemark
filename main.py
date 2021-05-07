a9154487d0f851f9b74e049c9fb1b9b5fc626bfb
Eden <eden1amir@gmail.com>
2021-05-07 15:34:47 +0300

    removed unusful comments

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import app_gui
 
 if __name__ == "__main__":
 
-    """
-    start_print = FireMark(
-        utils.printing_option(), utils.quantity(), utils.opacity(
-        ), utils.get_file_path_from_user(), utils.get_dir_path_from_user()
-    )
-    start_print.watermark_option()
-    """
-
     root = tk.Tk()
     root.geometry("1500x750")
     app_gui.GUI(root).pack(side="top", fill="both", expand=True)
