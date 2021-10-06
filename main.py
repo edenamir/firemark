5fa1f4948b14807e13498a3e05058841d78ac930
Eden <eden1amir@gmail.com>
2021-10-06 13:32:38 +0300

    Added a title and icon

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     # Open app in full screen
     root.state('zoomed')
+    root.title("Firemark")
+    root.iconbitmap(r"C:\Workspace\fire_mark\gui\favicon.ico")
     app_gui.GUI(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
